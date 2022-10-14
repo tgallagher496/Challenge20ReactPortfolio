ed5d19ea6d3361fa414872fb9cc8b7cc5201612e
tgallagher496 <tgallagher496@gmail.com>
2022-10-14 19:23:14 -0400

    Contact Page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../../assets/stylesheet.css";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const emailChange = document.querySelector("#emailInput");
+  var matcher = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+    return name === "firstName" ? setName(value) : setEmail(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // Alert the user their first and last name, clear the inputs
+    // alert(`Hello ${firstName} ${lastName}`);
+    setName("");
+    setEmail("");
+  };
+
   return (
     <div>
       <h1>Contact Page</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        <div>
+          <p>Hello {name}</p>
+          <form className="form">
+            <input
+              value={name}
+              name="firstName"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="First Name"
+            />
+            <input
+              value={email}
+              name="email"
+              id="emailInput"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="email"
+            />
+            <p id="response"></p>
+            <button type="button" onClick={handleFormSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
       </p>
     </div>
   );
